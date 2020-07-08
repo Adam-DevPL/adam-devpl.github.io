cd3ccf24f4e235ecdd7d768e547adbdc2ed5a480
Adam-DevPL <61238271+Adam-DevPL@users.noreply.github.com>
2020-07-08 12:39:00 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../scss/main.scss';
+
+// uncomment the lines below to enable PWA
+// import {registerSW} from './pwa.js';
+// registerSW();
+
+/* place your code below */
+
+console.log('HELLO 🚀')
